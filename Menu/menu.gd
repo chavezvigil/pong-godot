9ac1d0a91c87f0c6c2065132131b446f7e22d846
Luis Chavez <ljosue.chavez@gmail.com>
2025-11-17 20:25:37 -0600

    Creating menu

---
+++ Menu/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit(0)
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Pong/pong.tscn")
--- Pong/pong.gd
+++ Pong/pong.gd
@@ -3,6 +3,7 @@ extends Node2D
 var CENTER = Vector2(640, 360)
 var player_score = 20
 var computer_score = 20
+var is_paused = false
 
 func _ready() -> void:
 	$ComputerScore.text = str(computer_score)
@@ -34,3 +35,8 @@ func set_game () :
 		computer_score = 20
 		$ComputerScore.text = str(computer_score)
 		$PlayerScore.text = str(player_score)	
+
+
+func _on_touch_screen_button_pressed() -> void:
+	is_paused = !is_paused
+	get_tree().paused = is_paused
